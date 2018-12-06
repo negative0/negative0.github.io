da93f314d686917da89dedaafd9eb6692bedec84
Chaitanya Bankanhal <bchaitanya15@gmail.com>
2018-12-06 15:34:37 +0530

    Added mail, corrections ver1.7

--- js/main.js
+++ js/main.js
@@ -220,46 +220,48 @@
     $('#contactForm').validate({
 
         /* submit via ajax */
-        submitHandler: function(form) {
+        submitHandler: function (form) {
+            form.submit();
 
-            var sLoader = $('#submit-loader');
 
-            $.ajax({
-
-                type: "POST",
-                url: "https://formspree.io/bchaitanya15@gmail.com",
-                data: $(form).serialize(),
-                beforeSend: function() {
-
-                    sLoader.fadeIn();
-
-                },
-                success: function(msg) {
-
-                    // Message was sent
-                    if (msg == 'OK') {
-                        sLoader.fadeOut();
-                        $('#message-warning').hide();
-                        $('#contactForm').fadeOut();
-                        $('#message-success').fadeIn();
-                    }
-                    // There was an error
-                    else {
-                        sLoader.fadeOut();
-                        $('#message-warning').html(msg);
-                        $('#message-warning').fadeIn();
-                    }
-
-                },
-                error: function() {
-
-                    sLoader.fadeOut();
-                    $('#message-warning').html("Something went wrong. Please try again.");
-                    $('#message-warning').fadeIn();
-
-                }
-
-            });
+            // var sLoader = $('#submit-loader');
+            //
+            // $.ajax({
+            //
+            //     type: "POST",
+            //     url: "https://formspree.io/bchaitanya15@gmail.com",
+            //     data: $(form).serialize(),
+            //     beforeSend: function() {
+            //
+            //         sLoader.fadeIn();
+            //
+            //     },
+            //     success: function(msg) {
+            //
+            //         // Message was sent
+            //         if (msg == 'OK') {
+            //             sLoader.fadeOut();
+            //             $('#message-warning').hide();
+            //             $('#contactForm').fadeOut();
+            //             $('#message-success').fadeIn();
+            //         }
+            //         // There was an error
+            //         else {
+            //             sLoader.fadeOut();
+            //             $('#message-warning').html(msg);
+            //             $('#message-warning').fadeIn();
+            //         }
+            //
+            //     },
+            //     error: function() {
+            //
+            //         sLoader.fadeOut();
+            //         $('#message-warning').html("Something went wrong. Please try again.");
+            //         $('#message-warning').fadeIn();
+            //
+            //     }
+            //
+            // });
         }
 
     });
